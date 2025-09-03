5ad8d16e0f4d7250bf1333f070b465297c68f793
Thaís Santos <thais_gtape@hotmail.com>
2025-09-03 18:45:14 -0300

    Confifurações de segurança

---
+++ src/main/java/br/com/aula/projetoCadastroUsuario/security/SecurityConfigurations.java
@@ -0,0 +1,21 @@
+package br.com.aula.projetoCadastroUsuario.security;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfigurations {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(req -> req.anyRequest().permitAll())
+                .build();
+    }
+
+}
